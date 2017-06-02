e1bc4b6c66af430c1f7bd954188bf966d6ceb47a
zqiao <zqiao@ebay.com>
2017-06-02 09:35:04 +0800

    add dev tensorboard

--- worker.py
+++ worker.py
@@ -209,7 +209,7 @@ def train():
         saver.save(s, checkpoint_path, global_step=current_step)
         print('checkpoint done')
       if current_step != 0 and current_step % args.eval_frequency == 0:
-        for x, y in tqdm(batch_iterator(task.read_devset(epochs=1), 512, 1)):
+        for x, y in tqdm(batch_iterator(task.read_devset(epochs=1), args.batch_size, 1)):
           dev_step(x,y)
 
 def main():
